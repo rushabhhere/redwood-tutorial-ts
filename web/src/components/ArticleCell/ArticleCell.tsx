887fd7310791f6d937ddc8855019820737928e7a
Rushabh Javeri <javeri.rushabh45@gmail.com>
2022-07-07 00:23:34 +0530

    Created article page to display a single post based on its id

--- web/src/components/ArticleCell/ArticleCell.tsx
+++ web/src/components/ArticleCell/ArticleCell.tsx
@@ -1,12 +1,12 @@
-import type { ArticlesQuery } from 'types/graphql'
+import type { FindArticleQuery, FindArticleQueryVariables } from 'types/graphql'
 
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import Article from 'src/components/Article'
 
 export const QUERY = gql`
-  query PostsQuery {
-    articles: posts {
+  query FindArticleQuery($id: Int!) {
+    article: post(id: $id) {
       id
       title
       body
@@ -19,16 +19,12 @@ export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => <div>Empty</div>
 
-export const Failure = ({ error }: CellFailureProps) => (
+export const Failure = ({
+  error,
+}: CellFailureProps<FindArticleQueryVariables>) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
-  return (
-    <ul>
-      {articles.map((article) => (
-        <Article article={article} key={article.id} />
-      ))}
-    </ul>
-  )
+export const Success = ({ article }: CellSuccessProps<FindArticleQuery>) => {
+  return <Article article={article} />
 }
