78fd24d5517f2c0195c3f5f5cd24b7dfe15ee3bb
Rushabh Javeri <javeri.rushabh45@gmail.com>
2022-07-10 17:01:10 +0530

    Styled AboutPage

--- web/src/pages/AboutPage/AboutPage.test.tsx
+++ web/src/pages/AboutPage/AboutPage.test.tsx
@@ -2,9 +2,6 @@ import { render } from '@redwoodjs/testing/web'
 
 import AboutPage from './AboutPage'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
-
 describe('AboutPage', () => {
   it('renders successfully', () => {
     expect(() => {
