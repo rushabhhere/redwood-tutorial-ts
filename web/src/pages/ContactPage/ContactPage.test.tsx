9cffae160976d2dea0f209993e6c32fe96bf878a
Rushabh Javeri <javeri.rushabh45@gmail.com>
2022-07-10 17:16:48 +0530

    Styled ContactPage

--- web/src/pages/ContactPage/ContactPage.test.tsx
+++ web/src/pages/ContactPage/ContactPage.test.tsx
@@ -2,9 +2,6 @@ import { render } from '@redwoodjs/testing/web'
 
 import ContactPage from './ContactPage'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
-
 describe('ContactPage', () => {
   it('renders successfully', () => {
     expect(() => {
